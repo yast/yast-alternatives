7dfe972468428b6905ca314d252f79abf127cc4b
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2016-07-20 10:38:08 +0100

    Update help text

--- src/lib/update-alternatives/UI/main_dialog.rb
+++ src/lib/update-alternatives/UI/main_dialog.rb
@@ -49,34 +49,38 @@ module UpdateAlternatives
     def help_handler
       Yast::Popup.LongText(
         _("Help"),
-        RichText(_("<p>Alternatives are used to manage differents versions of an application. " \
-          "For example you can have differents versions of java installed on your system, " \
-          "and use the latest one when \"java\" is called.</p><p>Every alternative have an " \
-          "status, it can be automatic or manual. In <b>automatic mode</b> the system set " \
-          "the choice with higher priority. Note that <b>priority</b> is set by the packages " \
-          "creators trying to provide a reasonable defaults. The <b>manual mode</b> is set " \
-          "when the user selects a choice manually.</p><p>To change the current choice of an " \
-          "alternative, select the alternative, click the <b>edit</b> button then select the " \
-          "desired choice in the table and click on the <b>set choice</b> button. If you want to " \
-          "have the alternative in automatic mode just click the <b>set automatic mode</b> " \
-          "button in the edit dialog.</p><p>Furthermore every choice have his own <b>slaves</b>, " \
-          "these will be applied in the system when the choice is selected.</p><p>Lets see an " \
-          "example from the man-pages. We have an alternative \"editor\" that have two choices." \
-          "The first choice \"/bin/ed\" with the slave:</p>" \
-          "<pre>  editor.1.gz /usr/share/man/man1/ed.1.gz</pre>"\
-          "<p>Another choice \"/usr/bin/vim\" with the following slaves:</p>"\
-          "<pre>  editor.1.gz /usr/share/man/man1/vim.1.gz\n" \
-          "  editor.fr.1.gz /usr/share/man/fr/man1/vim.1.gz\n" \
-          "  editor.it.1.gz /usr/share/man/it/man1/vim.1.gz\n" \
-          "  editor.pl.1.gz /usr/share/man/pl/man1/vim.1.gz\n" \
-          "  editor.ru.1.gz /usr/share/man/ru/man1/vim.1.gz\n</pre>" \
-          "<p>In this case if we select the second choice all his slaves will be modified, " \
-          "or created if not exist already. Then if we set \"/bin/ed\" the slave " \
-          "\"editor.1.gz\" will be modified and the others will be removed.</p>"
-          )),
+        RichText(help_text),
         60,
         20
       )
+    end
+
+    def help_text
+      _("<p>The alternatives system is used when some equivalent applications are " \
+        "installed in the system. For example if several implementations and versions of Java " \
+        "are installed, the alternatives system will be used to decide which one to use when " \
+        "the command \"java\" is called.</p><p>Every alternative have an " \
+        "status, it can be automatic or manual. In <b>automatic mode</b> the system uses " \
+        "the choice with higher priority. Note that <b>priority</b> is set by the packages " \
+        "creators trying to provide reasonable defaults. The <b>manual mode</b> is set " \
+        "when the user selects a choice manually.</p><p>To change the current choice of an " \
+        "alternative, select the alternative, click the <b>edit</b> button then select the " \
+        "desired choice in the table and click on the <b>set choice</b> button. If you want to " \
+        "have the alternative in automatic mode just click the <b>set automatic mode</b> button " \
+        "in the edit dialog.</p><p>Furthermore every choice has its own set of <b>slaves</b>, " \
+        "these will be applied in the system when the choice is selected.</p><p>Lets see an " \
+        "example from the man-pages. We have an alternative \"editor\" that have two choices." \
+        "The first choice \"/bin/ed\" with the slave:</p>" \
+        "<pre>  editor.1.gz /usr/share/man/man1/ed.1.gz</pre>"\
+        "<p>Another choice \"/usr/bin/vim\" with the following slaves:</p>"\
+        "<pre>  editor.1.gz /usr/share/man/man1/vim.1.gz\n" \
+        "  editor.fr.1.gz /usr/share/man/fr/man1/vim.1.gz\n" \
+        "  editor.it.1.gz /usr/share/man/it/man1/vim.1.gz\n" \
+        "  editor.pl.1.gz /usr/share/man/pl/man1/vim.1.gz\n" \
+        "  editor.ru.1.gz /usr/share/man/ru/man1/vim.1.gz\n</pre>" \
+        "<p>In this case if the second choice is selected all its slaves will be modified, " \
+        "or created if they do not exist already. If the \"/bin/ed\" choice is set afterward, " \
+        "the slave \"editor.1.gz\" will be modified and the others will be removed.</p>")
     end
 
     def edit_alternative_handler
